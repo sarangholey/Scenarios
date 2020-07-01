e9c46ec9b7a85a68c8c8cbf04bad1c1627c3fa97
DishaniReddy <dishanireddy.rd@gmail.com>
2020-07-01 10:20:26 +0530

    Pushing code for Scenario Two and modified code in Scenario One. Both implemented using TestNG

--- src/test/java/scenarios/scn_2/AssignmentTwoScenarioTwo.java
+++ src/test/java/scenarios/scn_2/AssignmentTwoScenarioTwo.java
@@ -1,4 +1,4 @@
-package scenarios.scn_1;
+package scenarios.scn_2;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -10,22 +10,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class AssignmentOneScenarioOne {
+
+public class AssignmentTwoScenarioTwo {
 
 	@Test
-	public void FlipkartAssignment() throws InterruptedException {
+	public void ScenarioTwo() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dishu\\Work-Selenium\\Drivers\\chromedriver_win32_v83\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 		driver.get("https://www.flipkart.com/");
+		driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();	//click on close button of the frame
 		String expectedTitle = "Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, expectedTitle);	//validated if page Title is displayed
 		System.out.println("Successfully validated Page Title");
-
-		driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();	//click on close button of the frame
 
 		//validating if searchbox is displayed
 		WebElement searchBox = driver.findElement(By.xpath("//input[@class='LM6RPg']"));
@@ -35,20 +35,18 @@ public class AssignmentOneScenarioOne {
 		}else {
 			System.out.println("SearchBox is not displayed");
 		}
-		searchBox.sendKeys("OnePlus");
-		driver.findElement(By.xpath("//button[@class='vh79eN']")).click();	//click on search button
-		List<WebElement> priceOfProducts = driver.findElements(By.xpath("//div[@class='_1HmYoV _35HD7C']//div[@class='_1vC4OE']"));
-		int size = priceOfProducts.size();
-		System.out.println(size);
-		int count = 0;
-		for(int i = 0; i < size; i++) {
-			String price = priceOfProducts.get(i).getText().replace("₹","").replace(",","");
-			int p = Integer.parseInt(price);
-			System.out.println("Price of " +  i + " product is :" + p);
-			count = count + p;
+		String actualSearchedValue = "Philips";
+		String expectedSearchedValue = "Philips";
+		searchBox.sendKeys(actualSearchedValue);
+		Thread.sleep(1500);
+		List<WebElement> autosuggestion = driver.findElements(By.xpath("//div[@class='_2kpuIQ _1rB4Ez']"));
+		System.out.println("Size of autosuggestion list is: " + autosuggestion.size());
+		for(int i = 1; i < autosuggestion.size(); i++) {
+			String autosuggestionText = driver.findElements(By.xpath("//li[@class='_1va75j']/div/a/div[@class='_2kpuIQ _1rB4Ez']")).get(i).getText();
+			System.out.println(autosuggestionText);
+			Assert.assertEquals(actualSearchedValue, expectedSearchedValue, "Searched string value did not match with the enterred keyword");
 		}
-		System.out.println("\nSum of All the Prices is: " + count);
 		driver.quit();
 	}
-
 }
+
