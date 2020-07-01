b6b6fa28f8ad711a68ca51f948be77373bc3e6f3
PritiJodh18 <pritisjodh@gmail.com>
2020-07-01 12:52:43 +0530

    Completed.

--- src/test/java/scenarios/scn_2/Assignment2.java
+++ src/test/java/scenarios/scn_2/Assignment2.java
@@ -1,4 +1,4 @@
-package scenarios.scn_1;
+package scenarios.scn_2;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -9,13 +9,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Assignment1 {
+public class Assignment2 {
 	WebDriver driver;
 	String url="https://www.flipkart.com/";
 	String title="Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
-	int add=0;
+	String Product="philips";
 	@Test
-	public void MethodAssignment1() throws InterruptedException {
+	public void MethodAssignment2() throws InterruptedException {
 	WebDriver driver=new ChromeDriver();
     driver.get(url);
     driver.manage().window().maximize();
@@ -24,30 +24,29 @@ public class Assignment1 {
     driver.manage().window().maximize();
     
 	driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();	
-    Thread.sleep(4000);
 	
     WebElement SearchBox =driver.findElement(By.xpath("//input[@class='LM6RPg']"));
     System.out.println("SearchBox is displays: "+SearchBox.isDisplayed());
-    SearchBox.sendKeys("OnePlus");
-    driver.findElement(By.xpath("//button[@class='vh79eN']")).click();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    List<WebElement>Prices=driver.findElements(By.xpath("//div[@class='_1HmYoV _35HD7C']//div[@class='_1vC4OE']"));
-    int n=Prices.size();	
-    System.out.println("TotalProduct: "+n);
+    SearchBox.sendKeys("Philips");
+    Thread.sleep(5000);
     
+    List<WebElement> Autosuggestion = driver.findElements(By.xpath("//a[@class='_2ja22P']"));	
+    System.out.println("Total number of Autosuggestion Product: "+Autosuggestion.size());
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);				
     int i=0;
-    while(i<Prices.size())
+    while(i<Autosuggestion.size())
     {
-    	String Name=Prices.get(i).getText().replace("₹","").replace(",","");
-    	System.out.println("ProductCost: "+Name);
-    	int convert=Integer.parseInt(Name);
-        add=add+convert;	
-        System.out.println("TotalCost: "+add);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);				
-        i++;
+	String product=Autosuggestion.get(i).getText();
+	boolean display= Autosuggestion.get(i).isDisplayed();
+	System.out.println(display);
+	if(product.startsWith("philips")) {
+    System.out.println("Product start with small letter: "+product);
+	}
+	if(product.startsWith("Philips")){
+		System.out.println("Product Start with Capital Letter: "+product);
+	}
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);				
+	i++;
     }
   }
 }
-                                          
-
-
---
+++ src/test/java/scenarios/scn_3/Assignment3.java
@@ -0,0 +1,70 @@
+package scenarios.scn_3;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Assignment3 {
+	WebDriver driver;
+	String url="https://www.flipkart.com/";
+	String title="Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
+	String Min="10000";
+	String Max="30000";
+	@Test
+	public void MethodAssignment2() throws InterruptedException {
+	WebDriver driver=new ChromeDriver();
+    driver.get(url);
+    driver.manage().window().maximize();
+    Assert.assertEquals(title,driver.getTitle());
+    System.out.println("Page title is: "+driver.getTitle());
+    driver.manage().window().maximize();
+    
+	driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();	
+	
+    WebElement SearchBox =driver.findElement(By.xpath("//input[@class='LM6RPg']"));
+    System.out.println("SearchBox is displays: "+SearchBox.isDisplayed());
+    SearchBox.sendKeys("samsung mobiles");
+    driver.findElement(By.xpath("//button[@class='vh79eN']")).click();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    
+    WebElement MinPrice=driver.findElement(By.xpath("//div[@class='_1qKb_B']//select[@class='fPjUPw']"));
+    System.out.println("MinPrice is visible: "+MinPrice.isDisplayed());
+    WebElement MaxPrice=driver.findElement(By.xpath("//div[@class='_1YoBfV']//select[@class='fPjUPw']"));
+    System.out.println("Maxprice is visible: "+MaxPrice.isDisplayed());
+    Thread.sleep(3000);
+    
+    Select Dropdown=new Select(MinPrice);
+    Dropdown.selectByValue(Min);
+    WebElement LowerPrice=driver.findElement(By.xpath("//div[@class='_1qKb_B']//option[@value='10000']"));
+    System.out.println("SelectedMinPrice: "+LowerPrice.getText());
+    Thread.sleep(3000);
+    
+    Select Dropdown2=new Select(MaxPrice);
+    Dropdown2.selectByValue(Max);
+    WebElement HigherPrice=driver.findElement(By.xpath("//div[@class='_1YoBfV']//option[@value='30000']"));
+    System.out.println("SelectedMaxPrice: "+HigherPrice.getText());
+    
+    Thread.sleep(3000);
+    driver.findElement(By.xpath("//div[text()='Price -- Low to High']")).click();;
+    
+    Thread.sleep(3000);
+    WebElement LowerPriceProduct= driver.findElement(By.xpath("//div[text()='Samsung Galaxy J4 (Gold, 16 GB)']"));
+    WebElement MinPriceOfMobile=driver.findElement(By.xpath("//div[text()='₹10,903']"));
+    System.out.println("LowerPriceMobile Name is: "+LowerPriceProduct.getText());
+    System.out.println("PriceOfMobile is: "+MinPriceOfMobile.getText());
+    
+    Thread.sleep(3000);
+    driver.findElement(By.xpath("//div[text()='Price -- High to Low']")).click();
+    
+    Thread.sleep(3000);
+    WebElement HigherPriceProduct=driver.findElement(By.xpath("//div[text()='Samsung Galaxy A8 Plus (Gold, 64 GB)']"));
+    WebElement MaxPriceOfMobile=driver.findElement(By.xpath("//div[text()='₹30,000']"));
+	System.out.println("HigherPriceMobile Name is: "+HigherPriceProduct.getText());
+	System.out.println("MaxPriceOfMobile is: "+MaxPriceOfMobile.getText());
+	}
+}
