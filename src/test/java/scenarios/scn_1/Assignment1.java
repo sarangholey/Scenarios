129a45b6ca62393f7d4fe0c53d3135b49fe1c58c
PritiJodh18 <pritisjodh@gmail.com>
2020-07-01 16:32:29 +0530

    Made Changes.

--- src/test/java/scenarios/scn_1/Assignment1.java
+++ src/test/java/scenarios/scn_1/Assignment1.java
@@ -10,26 +10,26 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Assignment1 {
-    WebDriver driver;
-    String url="https://www.flipkart.com/";
-    String title="Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
-    int add=0;
-    @Test
-    public void MethodAssignment1() throws InterruptedException {
-    WebDriver driver=new ChromeDriver();
+	WebDriver driver;
+	String url="https://www.flipkart.com/";
+	String title="Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
+	int add=0;
+	
+	@Test
+	public void MethodAssignment1() throws InterruptedException {
+	WebDriver driver=new ChromeDriver();
     driver.get(url);
     driver.manage().window().maximize();
-    Assert.assertEquals(title,driver.getTitle());
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     System.out.println("Page title is: "+driver.getTitle());
-    
-    driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();	
-    Thread.sleep(4000);
-	
+    Assert.assertEquals(title,driver.getTitle());
+     
+	driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();	
+   
     WebElement SearchBox =driver.findElement(By.xpath("//input[@class='LM6RPg']"));
     System.out.println("SearchBox is displays: "+SearchBox.isDisplayed());
     SearchBox.sendKeys("OnePlus");
     driver.findElement(By.xpath("//button[@class='vh79eN']")).click();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     List<WebElement>Prices=driver.findElements(By.xpath("//div[@class='_1HmYoV _35HD7C']//div[@class='_1vC4OE']"));
     int n=Prices.size();	
     System.out.println("TotalProduct: "+n);
--- src/test/java/scenarios/scn_2/Assignment2.java
+++ src/test/java/scenarios/scn_2/Assignment2.java
@@ -8,44 +8,47 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Assignment2 {
-    WebDriver driver;
-    String url="https://www.flipkart.com/";
-    String title="Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
-    String Product="philips";
-    @Test
-    public void MethodAssignment2() throws InterruptedException {
-    WebDriver driver=new ChromeDriver();
+	WebDriver driver;
+	String url="https://www.flipkart.com/";
+	String title="Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
+	String Product="philips";
+	
+	@Test
+	public void MethodAssignment2() throws InterruptedException {
+	WebDriver driver=new ChromeDriver();
     driver.get(url);
     driver.manage().window().maximize();
-    Assert.assertEquals(title,driver.getTitle());
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     System.out.println("Page title is: "+driver.getTitle());
-    driver.manage().window().maximize();
+    Assert.assertEquals(title,driver.getTitle());
     
-    driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();	
+	driver.findElement(By.xpath("//button[@class='_2AkmmA _29YdH8']")).click();	
 	
     WebElement SearchBox =driver.findElement(By.xpath("//input[@class='LM6RPg']"));
     System.out.println("SearchBox is displays: "+SearchBox.isDisplayed());
     SearchBox.sendKeys("Philips");
-    Thread.sleep(5000);
     
+    WebDriverWait wait = new WebDriverWait(driver,30);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='_2ja22P']")));
     List<WebElement> Autosuggestion = driver.findElements(By.xpath("//a[@class='_2ja22P']"));	
     System.out.println("Total number of Autosuggestion Product: "+Autosuggestion.size());
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);				
     int i=0;
     while(i<Autosuggestion.size())
     {
 	String product=Autosuggestion.get(i).getText();
 	boolean display= Autosuggestion.get(i).isDisplayed();
 	System.out.println(display);
-	if(product.startsWith("philips")) {
+	
+	if(product.contains("philips")) {
     System.out.println("Product start with small letter: "+product);
 	}
-	if(product.startsWith("Philips")){
-		System.out.println("Product Start with Capital Letter: "+product);
+	if(product.contains("Philips")){
+	System.out.println("Product Start with Capital Letter: "+product);
 	}
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);				
 	i++;
     }
   }
