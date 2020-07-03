b9394f347cd9b67b96de276b6aa9f3bf347d1c9d
PritiJodh18 <60691242+PritiJodh18@users.noreply.github.com>
2020-07-03 15:11:58 +0530

    Update Assignment4.java

--- src/test/java/scenarios/scn_4/Assignment4.java
+++ src/test/java/scenarios/scn_4/Assignment4.java
@@ -14,13 +14,13 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Assignment4 {
-	WebDriver driver;
-	String url="https://www.amazon.in/";
-	String title="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+    WebDriver driver;
+    String url="https://www.amazon.in/";
+    String title="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
 
-	@Test
-	public void MethodAssignment4() throws InterruptedException {
-	WebDriver driver=new ChromeDriver();
+    @Test
+    public void MethodAssignment4() throws InterruptedException {
+    WebDriver driver=new ChromeDriver();
     driver.get(url);
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -44,12 +44,9 @@ public class Assignment4 {
     String childwin= it.next();
 	driver.switchTo().window(childwin);
 	
-	if(driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).isDisplayed()){
-	driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).click();}
-	else if(driver.findElement(By.xpath("//a[@id='mbc-buybutton-addtocart-1-announce']")).isDisplayed());{
-	driver.findElement(By.xpath("//a[@id='mbc-buybutton-addtocart-1-announce']")).click();}
-	
-	Thread.sleep(3000);
+    driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).isDisplayed()
+    driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).click();
+    Thread.sleep(3000);
     driver.close();
     driver.switchTo().window(parentwin);
     driver.navigate().refresh();
